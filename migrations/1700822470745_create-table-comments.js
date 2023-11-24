80c828919ccbf2af2a4980cbaef9573d75e2e17f
rickyaditb <rickyaditb@gmail.com>
2023-11-24 17:52:00 +0700

    feat: added threads and table migration

---
+++ migrations/1700822470745_create-table-comments.js
@@ -0,0 +1,34 @@
+/* eslint-disable camelcase */
+exports.up = (pgm) => {
+  pgm.createTable('comments', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    thread_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'threads',
+      onDelete: 'CASCADE',
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+      references: 'users',
+      onDelete: 'CASCADE',
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    date: {
+      type: 'TIMESTAMP',
+      notNull: true,
+      default: pgm.func('current_timestamp'),
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('comments');
+};
